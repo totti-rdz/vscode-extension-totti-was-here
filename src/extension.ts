14e92354135e36a35111cc7a02b4ae33f8bf1549
Totti Rdz <thorsten_herfurtner@gmx.de>
2024-09-23 01:28:59 +0200

    refactor: command registration

--- src/extension.ts
+++ src/extension.ts
@@ -3,23 +3,22 @@ import * as vscode from "vscode";
 export function activate(context: vscode.ExtensionContext) {
   console.log('extension "tottiwashere" is now active!');
 
-  const disposable = vscode.commands.registerCommand(
-    "tottiwashere.helloWorld",
-    () => {
+  const disposables: vscode.Disposable[] = [];
+
+  disposables.push(
+    vscode.commands.registerCommand("tottiwashere.helloWorld", () => {
       vscode.window.showInformationMessage("Hello World!");
-    }
+    })
   );
 
-  const disposable2 = vscode.commands.registerCommand(
-    "tottiwashere.helloAgain",
-    () => {
+  disposables.push(
+    vscode.commands.registerCommand("tottiwashere.helloAgain", () => {
       vscode.window.showInformationMessage("Hello Again!");
-    }
+    })
   );
 
-  const disposable3 = vscode.commands.registerCommand(
-    "tottiwashere.newPanel",
-    () => {
+  disposables.push(
+    vscode.commands.registerCommand("tottiwashere.newPanel", () => {
       const panel = vscode.window.createWebviewPanel(
         "tottiwashere",
         "Totti was here",
@@ -28,12 +27,10 @@ export function activate(context: vscode.ExtensionContext) {
       );
 
       panel.webview.html = "<h1>Totti was here</h1>";
-    }
+    })
   );
 
-  context.subscriptions.push(disposable);
-  context.subscriptions.push(disposable2);
-  context.subscriptions.push(disposable3);
+  disposables.forEach((disposable) => context.subscriptions.push(disposable));
 }
 
 export function deactivate() {}
