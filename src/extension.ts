21a970d3f9ec4f9bf4d2094a43930a2a9899e909
Totti Rdz <thorsten_herfurtner@gmx.de>
2024-09-23 00:57:15 +0200

    prettier

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,24 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
+  console.log('extension "tottiwashere" is now active!');
 
-	console.log('extension "tottiwashere" is now active!');
+  const disposable = vscode.commands.registerCommand(
+    "tottiwashere.helloWorld",
+    () => {
+      vscode.window.showInformationMessage("Hello World!");
+    }
+  );
 
-	const disposable = vscode.commands.registerCommand('tottiwashere.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World!');
-	});
+  const disposable2 = vscode.commands.registerCommand(
+    "tottiwashere.helloAgain",
+    () => {
+      vscode.window.showInformationMessage("Hello Again!");
+    }
+  );
 
-	const disposable2 = vscode.commands.registerCommand('tottiwashere.helloAgain', () => {
-		vscode.window.showInformationMessage('Hello Again!');
-	});
-
-	context.subscriptions.push(disposable);
-	context.subscriptions.push(disposable2);
+  context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable2);
 }
 
 export function deactivate() {}
