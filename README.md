a5d674a31cb357585ef0fc7270ed57048ccdd14f
Totti Rdz <thorsten_herfurtner@gmx.de>
2024-09-22 23:36:39 +0200

    refactor: remove "vscodex-" prefix from extension name

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# vscodex-tottiwashere README
+# tottiwashere README
 
-This is the README for your extension "vscodex-tottiwashere". After writing up a brief description, we recommend including the following sections.
+This is the README for your extension "tottiwashere". After writing up a brief description, we recommend including the following sections.
 
 ## Features
 
--- src/extension.ts
+++ src/extension.ts
@@ -2,13 +2,13 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	console.log('extension "vscodex-tottiwashere" is now active!');
+	console.log('extension "tottiwashere" is now active!');
 
-	const disposable = vscode.commands.registerCommand('vscodex-tottiwashere.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('tottiwashere.helloWorld', () => {
 		vscode.window.showInformationMessage('Hello World!');
 	});
 
-	const disposable2 = vscode.commands.registerCommand('vscodex-tottiwashere.helloAgain', () => {
+	const disposable2 = vscode.commands.registerCommand('tottiwashere.helloAgain', () => {
 		vscode.window.showInformationMessage('Hello Again!');
 	});
 
